eeff1dffe237a76a005c81c694cb2040de88d4be
neelneelpurk <soumyadeep1999.np@gmail.com>
2018-05-07 03:10:23 +0530

    added datatable child class

--- hello2.js
+++ hello2.js
@@ -1,18 +1,41 @@
 
+/* Formatting function for row details - modify as you need */
+function format ( d ) {
+    console.log(d.P_ASIN);
+    var trs='';
+    $.each($(d.P_ASIN),function(key,value){
+    	trs+='<tr><td>'+value+'</td><td>'+d.P_Title[key]+'</td><td>'+d.P_SalesRank[key]+'</td><td>'+d.P_Pages[key]+'</td><td>'+d.P_Price[key]+'</td><td>'+d.P_Review[key]+'</td><td>'+d.P_Rating[key]+'</td></tr>';
+    })
+    // `d` is the original data object for the row
+    return '<table class="table table-border table-hover">'+
+           '<thead>'+
+              '<th>ASIN</th>'+
+            	'<th>Title</th>'+
+            	'<th>Salesrank</th>'+
+              '<th>No of Pages</th>'+
+            	'<th>Price</th>'+
+              '<th>Review</th>'+
+            	'<th>Rating</th>'+  
+           '</thead><tbody>' +
+           	
+           trs+
+        '</tbody></table>';
+}
+
 $(document).ready(function() {
- $("button").click(function(){
-    var url0 = "https://keyword-spy.herokuapp.com/keyword?q=";
-    var url1 = url0 + $("input:text").val()
-    content = "<tr><th>Keyword</th><th>Price</th><th>Rating</th><th>Review</th><th>Salesrank</th><th>Score</th></tr>";
+  $("button").click(function(){
+    content = "<tr><th></th><th>Keyword</th><th>Price</th><th>Rating</th><th>Review</th><th>Salesrank</th><th>Score</th></tr>";
     $('#heads').html(content);
-    $('#example').DataTable( {
+    var table = $('#example').DataTable({
         "destroy": true,
-        'ajax': {
-        "crossDomain": true,
-        "url"    : url1,
-        "dataSrc": ""
-        },
+        "ajax": "https://keyword-spy.herokuapp.com/keyword?q="+$("input:text").val(),
         "columns": [
+            {
+                "class":          'details-control',
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": ''
+            },
             { "data": "keyword" },
             { "data": "price" },
             { "data": "rating" },
@@ -27,6 +50,23 @@ $(document).ready(function() {
             'csvHtml5',
             'pdfHtml5'
         ]
-} );
-} );
-} );
+    });
+
+    // Add event listener for opening and closing details
+   $('#example tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table.row( tr );
+
+        if ( row.child.isShown() ) {
+          // This row is already open - close it
+          row.child.hide();
+          tr.removeClass('shown');
+        }
+        else {
+          // Open this row
+          row.child( format(row.data()) ).show();
+          tr.addClass('shown');
+        }
+    });
+});
+});
