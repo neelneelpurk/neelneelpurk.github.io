82157585964b0cc3c3a1847b808c0dcbd009ecc8
neelneelpurk <soumyadeep1999.np@gmail.com>
2018-07-14 13:21:51 +0530

    hello4

--- hello2.js
+++ hello2.js
@@ -1,72 +1,148 @@
-
-/* Formatting function for row details - modify as you need */
-function format ( d ) {
-    console.log(d.P_ASIN);
-    var trs='';
-    $.each($(d.P_ASIN),function(key,value){
-    	trs+='<tr><td>'+value+'</td><td>'+d.P_Title[key]+'</td><td>'+d.P_SalesRank[key]+'</td><td>'+d.P_Pages[key]+'</td><td>'+d.P_Price[key]+'</td><td>'+d.P_Review[key]+'</td><td>'+d.P_Rating[key]+'</td></tr>';
-    })
-    // `d` is the original data object for the row
-    return '<table class="table table-border table-hover">'+
-           '<thead>'+
-              '<th>ASIN</th>'+
-            	'<th>Title</th>'+
-            	'<th>Salesrank</th>'+
-              '<th>No of Pages</th>'+
-            	'<th>Price(€)</th>'+
-              '<th>Review</th>'+
-            	'<th>Rating</th>'+  
-           '</thead><tbody>' +
-           	
-           trs+
-        '</tbody></table>';
-}
-
 $(document).ready(function() {
-  $("button").click(function(){
-    content = "<tr><th></th><th>Keyword</th><th>Price(€)</th><th>Rating</th><th>Review</th><th>Salesrank</th><th>Score</th></tr>";
-    $('#heads').html(content);
-    var table = $('#example').DataTable({
-        "destroy": true,
-        "ajax": "https://keyword-spy.herokuapp.com/keyword?q="+$("input:text").val(),
-        "columns": [
-            {
-                "class":          'details-control',
-                "orderable":      false,
-                "data":           null,
-                "defaultContent": ''
-            },
-            { "data": "keyword" },
-            { "data": "price" },
-            { "data": "rating" },
-            { "data": "review" },
-            { "data": "salesrank" },
-            { "data": "score" }
-        ],
-        dom: 'Bfrtip',
-        buttons: [
-            'copyHtml5',
-            'excelHtml5',
-            'csvHtml5',
-            'pdfHtml5'
-        ]
-    });
+    $("button").click(function(){
+	var url1 =$("input:text").val()
+        var url2 = "";
+    	var url = url2+"https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Ddigital-text&field-keywords="+url1;
+		$.get(url, function(response) {
+			//console.log(response);
+			var patt=/data-asin/g;
+			while (patt.test(response)==true){
+				var index=patt.lastIndex;
+				if(response[index+2]=='B'){
+					var i;
+					var str="";
+					for(i=2; i<12; i++){
+						str+=response[index+i];
+		  		    }
+		  		    console.log(str);
+		  		    var url1= url2 +"https://www.amazon.com/dp/"+str+"/";
+		            $.get(url1, function(response1){
+                        //console.log(response1);
+			var content = "";
+                        var title=/a-size-extra-large/g;
+                        var j=0;
+                        while(title.test(response1)==true){
+						    var index1=title.lastIndex;
+						    index1+=2;
+						    //console.log(index1);
+						    j=j+1;
+						    var titlestr="";
+		                    while(response1[index1]!="<" && j==4){
+		                      	titlestr+=response1[index1];
+		                       	index1=index1+1;
+		                    }
+		                    if(j==4){
+		                        console.log(titlestr);
+					content += "<tr class='table-info'><td>"+titlestr+"</td>";
+					}
+				        } 
+				        var review=/totalReviewCount/g;
+				        while(review.test(response1)==true){
+						    var index1=review.lastIndex;
+						    index1+=2;
+						    //console.log(index1);
+						    var reviewstr="";
+		                    while(response1[index1]!="<"){
+		                      	reviewstr+=response1[index1];
+		                       	index1=index1+1;
+		                    }
+				    content += "<td>"+reviewstr+"</td>";
+		                    console.log(reviewstr);
+				        }  
+				        var rat=/Length:</g;
+				        while(rat.test(response1)==true){
+						    var index1=rat.lastIndex;
+						    index1+=4;
+						    //console.log(index1);
+						    var ratstr="";
+		                    while(response1[index1]!="<"){
+		                      	ratstr+=response1[index1];
+		                       	index1=index1+1;
+		                    }
+				    var ratstr1 = ratstr.split(" ");
+		                    console.log(ratstr1[0]);
+				    content += "<td>"+ratstr1[0]+"</td>";
+				        } 
+				        var bsr=/Amazon Best Sellers Rank:</g;
+				        while(bsr.test(response1)==true){
+						    var index1=bsr.lastIndex;
+						    index1+=3;
+						    //console.log(index1);
+						    var bsrstr="";
+		                    while(response1[index1]!="("){
+		                      	bsrstr+=response1[index1];
+		                       	index1=index1+1;
+		                    }
+				    var bsrstr1 = bsrstr.trimLeft().split(" ");
+		                    console.log(bsrstr1[0]);
+				    content += "<td>"+bsrstr1[0]+"</td>";
+				        } 
+				        var pr=/a-color-price a-color-price">/g;
+				        while(pr.test(response1)==true){
+						    var index1=pr.lastIndex;
+						    //console.log(index1);
+						    var prstr="";
+		                    while(response1[index1]!="<"){
+		                      	prstr+=response1[index1];
+		                       	index1=index1+1;
+		                    }
+		                    console.log(prstr.trimLeft());
+				    content += "<td>"+prstr.trimLeft()+"</td>";
+				        } 
+				var lang=/<b>Language:</g;
+                        while(lang.test(response1)==true){
+                            var index1=lang.lastIndex+4;
+                            var langstr="";
+                            while(response1[index1]!="<"){
+                                langstr+=response1[index1];
+                                index1=index1+1;
+                            }
+                            console.log(langstr);
+			    content += "<td>"+langstr+"</td>";
+                        }
+                        var publish=/<b>Publisher:</g;
+                        while(publish.test(response1)==true){
+                            var index1=publish.lastIndex+4;
+                            var publishstr="";
+                            while(response1[index1]!="<"){
+                                publishstr+=response1[index1];
+                                index1=index1+1;
+                            }
+                            console.log(publishstr);
+			    content += "<td>"+publishstr+"</td>";
+                        }
 
-    // Add event listener for opening and closing details
-   $('#example tbody').on('click', 'td.details-control', function () {
-        var tr = $(this).closest('tr');
-        var row = table.row( tr );
+				        var rr=/class="arp-rating-out-of-text">/g;
+				        while(rr.test(response1)==true){
+						    var index1=rr.lastIndex;
+						    //console.log(index1);
+						    var rrstr="";
+		                    while(response1[index1]!="<"){
+		                      	rrstr+=response1[index1];
+		                       	index1=index1+1;
+		                    }
+				    var rrstr1 = rrstr.split(" ");
+		                    console.log(rrstr1[0]);
+				    content += "<td>"+rrstr1[0]+"</td>";
+				    content += "</tr>";
+				    console.log(content);
+				    $('#suggested_Result_box').append(content);
+				        }
+				        /*var lang=//g;
+				        while(lang.test(response1)==true){
+				        	var index1=lang.lastIndex;
+				            var langstr="";
+				            while(response1[index1]!="<"){
+                                langstr+=response1[index1];
+                                index1=index1+1;
+				            }
+				            console.log(langstr);
+				        }
+				        */
+  		            });
+		  		}
+		    }
+		});
+	});
+});
 
-        if ( row.child.isShown() ) {
-          // This row is already open - close it
-          row.child.hide();
-          tr.removeClass('shown');
-        }
-        else {
-          // Open this row
-          row.child( format(row.data()) ).show();
-          tr.addClass('shown');
-        }
-    });
-});
-});
